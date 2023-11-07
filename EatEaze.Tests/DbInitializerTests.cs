9e2db259ad1031a0094020527950d70aea17e513
tima999 <tima.antya@mail.ru>
2023-11-07 18:24:26 +0300

    Added DbHelper and unit test

---
+++ EatEaze.Tests/DbInitializerTests.cs
@@ -0,0 +1,20 @@
+﻿using EatEaze.DbHelpers;
+
+namespace EatEaze.Tests
+{
+    public class DbInitializerTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+        [Test]
+        public void DbInitializerTest()
+        {
+            var result = DbInitializer.InitDatabase();
+            Assert.That(result, Is.EqualTo(0));
+        }
+    }
+}
--- EatEaze/DbHelpers/DbInitializer.cs
+++ EatEaze/DbHelpers/DbInitializer.cs
@@ -1,15 +1,81 @@
 ﻿using EatEaze.Data.DataContext;
+using EatEaze.Data.Entities;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+using System.Text.Json;
 
 namespace EatEaze.DbHelpers
 {
+    /// <summary>
+    /// class for initializing database with data
+    /// </summary>
     public static class DbInitializer
     {
         private static string[] _opt = { };
         private static EatEazeDataContext _eatEazeDataContext = new EatEazeDataContextDesignTimeFactory().CreateDbContext(_opt);
 
-        public static void InitDatabase()
+        /// <summary>
+        /// provides DbSets with data from json files.
+        /// </summary>
+        /// <returns>0 if everything is okay, 1 if some data deserialized wrong</returns>
+        public static int InitDatabase()
         {
-            
+            if (!_eatEazeDataContext.Categories.Any())
+            {
+                string path = Path.Combine(@".\DataFiles", "Categories.json");
+                string json = File.ReadAllText(path);
+                var categories = JsonSerializer.Deserialize<List<Category>>(json);
+
+                if (categories == null)
+                {
+                    return 1;
+                }
+
+                _eatEazeDataContext.Categories.AddRange(categories);
+            }
+
+            if (!_eatEazeDataContext.Cities.Any())
+            {
+                string path = Path.Combine(@".\DataFiles", "Cities.json");
+                string json = File.ReadAllText(path);
+                var cities = JsonSerializer.Deserialize<List<City>>(json);
+
+                if (cities  == null)
+                {
+                    return 1;
+                }
+
+                _eatEazeDataContext.Cities.AddRange(cities);
+            }
+
+            if (!_eatEazeDataContext.Restaraunts.Any())
+            {
+                string path = Path.Combine(@".\DataFiles", "Restaraunts.json");
+                string json = File.ReadAllText(path);
+                var restaraunts = JsonSerializer.Deserialize<List<Restaraunt>>(json);
+
+                if (restaraunts == null)
+                {
+                    return 1;
+                }
+
+                _eatEazeDataContext.Restaraunts.AddRange(restaraunts);
+            }
+
+            if (!_eatEazeDataContext.RestarauntsInCities.Any())
+            {
+                string path = Path.Combine(@".\DataFiles", "RestarauntsInCities.json");
+                string json = File.ReadAllText(path);
+                var restarauntsInCities = JsonSerializer.Deserialize<List<RestarauntInCity>>(json);
+
+                if (restarauntsInCities == null)
+                {
+                    return 1;
+                }
+
+                _eatEazeDataContext.RestarauntsInCities.AddRange(restarauntsInCities);
+            }
+
+            return 0;
         }
     }
 }
