f9d431098b5b9d497f3a61a4ee5ece73ecc36d99
tima999 <tima.antya@mail.ru>
2023-11-10 16:32:56 +0300

    some unit tests

---
+++ EatEaze.Tests/PositionsServiceTests.cs
@@ -0,0 +1,28 @@
+﻿using EatEazeServices.Interfaces;
+using EatEazeServices.Implementations;
+using EatEaze.Data.Repositiories;
+using EatEaze.Data.DataContext;
+
+namespace EatEaze.Tests
+{
+    public class PositionsServiceTests
+    {
+        private IPositionsService _positionsService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            string[] opt = { };
+            EatEazeDataContext dataContext = new EatEazeDataContextDesignTimeFactory().CreateDbContext(opt);
+            PositionsRepository pr = new PositionsRepository(dataContext);
+            _positionsService = new PositionsService(pr);
+        }
+
+        [Test]
+        public void GetPositionsTest()
+        {
+            var result = _positionsService.GetPositions().Result;
+            Assert.That(!result.Equals(null));
+        }
+    }
+}
