2fb1364df15ffa0a511410460ed8162719be95d7
Тимофей <tima.antya@mail.ru>
2023-12-11 17:31:08 +0300

    /

---
+++ EatEaze.WebApplicationAPI/Controllers/CityController.cs
@@ -0,0 +1,26 @@
+﻿using EatEaze.Data.Repositiories.RepositoriesInterfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EatEaze.WebApplicationAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CityController : ControllerBase
+    {
+        private ICitiesRepository _cityRepository;
+
+        public CityController(ICitiesRepository citiesRepository)
+        {
+            _cityRepository = citiesRepository;
+        }
+
+        [HttpGet, Route("cities")]
+        public async Task<IActionResult> GetCities()
+        {
+            var result = await _cityRepository.GetListOfItem();
+            if (result == null) return NotFound();
+            return Ok(result);
+        }
+    }
+}
